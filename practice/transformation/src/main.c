244abd79472a752fca2105889931c951111c5a05
hyungraelee <timgg12345@gmail.com>
2021-02-25 22:43:33 +0900

    modifying hit square

--- practice/transformation/src/main.c
+++ practice/transformation/src/main.c
@@ -6,7 +6,7 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 12:51:31 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/02/24 18:51:18 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/02/25 18:26:47 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_scene		*scene_init(void)
 	if (!(scene = (t_scene *)malloc(sizeof(t_scene))))
 		return (NULL);
 	scene->canvas = canvas(700, 700);
-	scene->camera = camera(&scene->canvas, point3(0, 2, 15));
+	scene->camera = camera(&scene->canvas, point3(0, 5, 15));
 	world = object(SP, sphere(point3(-2, 2, -5), 2), color3(0.5, 0, 0));
 	// world = object(CY, cylinder(point3(6, -3, -7), vec3(0, 1, 0), 5.0, 6.0), color3(0.9, 0, 0.4));
 	// world = object(SQ, square(point3(-10, 2, -2), vec3(0, 1, 0), 4), color3(0.4, 1, 0.4));
@@ -40,9 +40,9 @@ t_scene		*scene_init(void)
 	oadd(&world, object(PL, plane(point3(0, 21, 0), vec3(0, 1, 0)), color3(0, 1, 0)));
 	oadd(&world, object(TR, triangle(point3(-2, 0, -5), point3(2, 0, 4), point3(0, 2, -3)), color3(0.5, 0.5, 0.5)));
 	// oadd(&world, object(CY, cylinder(point3(6, -3, -7), vec3(1, 2, -3), 5.0, 6.0), color3(0.9, 0, 0.4)));
-	oadd(&world, object(SQ, square(point3(-10, 0, 0), vec3(0, 0, 1), 4), color3(0.2, 0.7, 0.3)));
-	oadd(&world, object(SQ, square(point3(-8, 0, -2), vec3(1, 0, 0), 4), color3(0.3, 0.8, 0.4)));
-	// oadd(&world, object(SQ, square(point3(-10, 2, -2), vec3(0, 1, 0), 4), color3(0.4, 1, 0.4)));
+	// oadd(&world, object(SQ, square(point3(-10, 0, 0), vec3(0, 0, 1), 4), color3(0.2, 0.7, 0.3)));
+	oadd(&world, object(SQ, square(point3(-8, 0, -2), vec3(1, 0, 0), 4), color3(0.9, 0.8, 0.4)));
+	oadd(&world, object(SQ, square(point3(-10, 2, -2), vec3(0, 1, 0), 4), color3(0.4, 1, 1)));
 	oadd(&world, object(SP, sphere(point3(2, 0, -5), 2), color3(0, 0.5, 0)));
 	scene->world = world;
 	lights = object(LIGHT_POINT, light_point(point3(0, 20, 5), color3(1, 1, 1), 0.5), color3(0, 0, 0)); // ?���? albedo
--- practice/transformation/src/trace/hit/hit_square.c
+++ practice/transformation/src/trace/hit/hit_square.c
@@ -6,10 +6,11 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 11:45:30 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/02/24 21:31:05 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/02/25 18:26:12 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "../../../include/utils.h"
 #include "../../../include/trace.h"
 
@@ -35,51 +36,89 @@ t_bool	hit_square(t_object *sq_obj, t_ray *ray, t_hit_record *rec)
 	if (root < rec->tmin || root > rec->tmax)
 		return (FALSE);
 	p = ray_at(ray, root);
-	// center와 normal이 만드는 평면에서 center의 z좌표와 같은 z값을 갖는 다른 두 점을 구한다.
 
-
-	// c = sq->normal.x / sq->normal.y;
-	// a = 1 + pow(c, 2);
-	// b = sq->center.x;
-	// sq_p1.x = b + sqrt(a * sq->length * sq->length) / (2 * a);
-	// sq_p1.y = (-1) * c * sqrt(sq->length * sq->length * a) / (2 * a) + sq->center.y;
-	// sq_p1.z = sq->center.z;
-
-	// sq_p2.x = b - sqrt(a * sq->length * sq->length) / (2 * a);
-	// sq_p2.y = c * sqrt(sq->length * sq->length * a) / (2 * a) + sq->center.y;
-	// sq_p2.z = sq->center.z;
+	/*
+	center와 normal이 만드는 평면에서 center의 z좌표와 같은 z값을 갖는 다른 두 점을 구한다.
+	z = center.z 인 평면으로 위의 평면을 자른다는 개념.
+	그럼
+	*/
 	gradient = -sq->normal.x / sq->normal.y;
 	theta = atan(gradient);
-	sq_p1.x = sq->center.x + cos(theta) * (cos(M_PI / 4) * sq->length);
-	sq_p1.y = sq->center.y + sin(theta) * (cos(M_PI / 4) * sq->length);
-	sq_p1.z = sq->center.z;
-	sq_p2.x = sq->center.x - cos(theta) * (cos(M_PI / 4) * sq->length);
-	sq_p2.y = sq->center.y + sin(theta) * (cos(M_PI / 4) * sq->length);
-	sq_p2.z = sq->center.z;
-
-	if (sq_p1.x < sq_p2.x)
+	if (sq->normal.x >= 0)
 	{
-		if (p.x < (sq_p1.x - EPSILON) || p.x > (sq_p2.x + EPSILON))
-			return (FALSE);
+		if (sq->normal.y > 0)
+		{
+			sq_p1.x = sq->center.x + cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.y = sq->center.y - sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.z = sq->center.z;
+			sq_p2.x = sq->center.x - cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.y = sq->center.y + sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.z = sq->center.z;
+		}
+		else
+		{
+			sq_p1.x = sq->center.x + cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.y = sq->center.y + sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.z = sq->center.z;
+			sq_p2.x = sq->center.x - cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.y = sq->center.y - sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.z = sq->center.z;
+		}
 	}
-	else if (sq_p1.x > sq_p2.x)
+	else if (sq->normal.x < 0)
+	{
+		if (sq->normal.y > 0)
+		{
+			sq_p1.x = sq->center.x + cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.y = sq->center.y + sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.z = sq->center.z;
+			sq_p2.x = sq->center.x - cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.y = sq->center.y - sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.z = sq->center.z;
+		}
+		else
+		{
+			sq_p1.x = sq->center.x + cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.y = sq->center.y - sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p1.z = sq->center.z;
+			sq_p2.x = sq->center.x - cos(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.y = sq->center.y + sin(theta) * ((sqrt(2) / 2) * sq->length);
+			sq_p2.z = sq->center.z;
+		}
+	}
+
+	if (sq_p1.x > sq_p2.x + EPSILON)
 	{
 		if (p.x > (sq_p1.x + EPSILON) || p.x < (sq_p2.x - EPSILON))
 			return (FALSE);
 	}
-
-	if (sq_p1.y < sq_p2.y)
+	else if (sq_p1.x + EPSILON < sq_p2.x)
 	{
-		if (p.y < (sq_p1.y - EPSILON) || p.y > (sq_p2.y + EPSILON))
+		if (p.x < (sq_p1.x - EPSILON) || p.x > (sq_p2.x + EPSILON))
 			return (FALSE);
 	}
-	else if (sq_p1.y > sq_p2.y)
+	else if (fabs(sq_p1.x - sq_p2.x) > EPSILON)
+	{
+		if (p.x < (sq_p1.x - EPSILON) || p.x > (sq_p1.x + EPSILON))
+			return (FALSE);
+	}
+
+	if (sq_p1.y > sq_p2.y + EPSILON)
 	{
 		if (p.y > (sq_p1.y + EPSILON) || p.y < (sq_p2.y - EPSILON))
 			return (FALSE);
 	}
+	else if (sq_p1.y + EPSILON < sq_p2.y)
+	{
+		if (p.y < (sq_p1.y - EPSILON) || p.y > (sq_p2.y + EPSILON))
+			return (FALSE);
+	}
+	else if (fabs(sq_p1.y - sq_p2.y) < EPSILON)
+	{
+		if (p.y < (sq_p1.y - EPSILON) || p.y > (sq_p1.y + EPSILON))
+			return (FALSE);
+	}
 
-	// if (sq_p1.z < sq_)
 	rec->t = root;
 	rec->p = p;
 	rec->normal = sq->normal;
