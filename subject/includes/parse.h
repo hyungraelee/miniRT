944bfdd93fbda2d6c6332875f0ef945f2a46ba23
hyungraelee <timgg12345@gmail.com>
2021-03-03 00:26:55 +0900

    add rendering

--- subject/includes/parse.h
+++ subject/includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 16:12:51 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/03/02 17:49:00 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/03/03 00:06:53 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,12 @@ int	parse_rt(char	*argv, t_scene *scene);
 int	handle_info(char **info, t_scene *scene);
 int	info_r(char	**info, t_scene *scene);
 int	info_c(char **info, t_scene *scene);
+int	info_l(char **info, t_scene *scene);
+int	info_sp(char **info, t_scene *scene, t_object **world);
+int	info_pl(char **info, t_scene *scene, t_object **world);
+int	info_sq(char **info, t_scene *scene, t_object **world);
+int	info_cy(char **info, t_scene *scene, t_object **world);
+int	info_tr(char **info, t_scene *scene, t_object **world);
 
 int	ft_isright_info(char c);
 int	check_info_err(char **info);
