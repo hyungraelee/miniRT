3bea26e827136869ba39e1a4617be09755bdf65c
hyungraelee <timgg12345@gmail.com>
2021-03-08 18:38:21 +0900

    del ft_strrev.c

--- subject/lib/libft/libft.h
+++ subject/lib/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 16:26:20 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/03/08 17:53:21 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/03/08 18:35:49 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,5 @@ t_list			*ft_lstmap(t_list *lst, \
 				void *(*f)(void *), void (*del)(void *));
 double			ft_atod(char *str);
 int				ft_issign(char c);
-char			*ft_strrev(char *str);
-int				ft_strcmp(char *s1, char *s2)
+int				ft_strcmp(char *s1, char *s2);
 #endif
--- subject/srcs/main.c
+++ subject/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hyunlee <hyunlee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 16:33:10 by hyunlee           #+#    #+#             */
-/*   Updated: 2021/03/08 18:05:24 by hyunlee          ###   ########.fr       */
+/*   Updated: 2021/03/08 18:34:37 by hyunlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,22 +31,18 @@ int main(int argc, char *argv[])
 	t_vars	vars;
 
 	vars.scene = scene_init();
-	if (argc == 2 && ft_strncmp(ft_strrev(argv[1]), "tr.", 3))
-	{
-		parse_rt(argv[1], vars.scene);
-		set_mlx(&vars);
-		rendering(&vars);
-		handle_hook(&vars);
-		mlx_loop(vars.mlx);
-	}
-	else if (argc == 3 && ft_strcmp(argv[1], "--save"))
-	{
-		if (ft_strncmp(ft_strrev(argv[2]), "tr.", 3))
-		{
-			parse_rt(argv[2], vars.scene);
-			rendering_bmp(vars);
-		}
-	}
+
+	parse_rt(argv[1], vars.scene);
+	set_mlx(&vars);
+	rendering(&vars);
+	handle_hook(&vars);
+	mlx_loop(vars.mlx);
+	// else if (argc == 3 && ft_strcmp(argv[1], "--save"))
+	// {
+
+	// 		parse_rt(argv[2], vars.scene);
+	// 		rendering_bmp(vars);
+	// }
 	// else
 	// 	err;
 	return (0);
